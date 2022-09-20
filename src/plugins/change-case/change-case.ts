29c3e3641f8b58812fa8a67664c75a4818f2fcbc
Sunwook KIm <sunus7@naver.com>
2022-09-20 23:22:50 +0900

    Fix: wrong text

--- src/plugins/change-case/change-case.ts
+++ src/plugins/change-case/change-case.ts
@@ -1,4 +1,4 @@
-ㅁ/*!
+/*!
  * Jodit Editor PRO (https://xdsoft.net/jodit/)
  * See LICENSE.md in the project root for license information.
  * Copyright (c) 2013-2022 Valeriy Chupurnov. All rights reserved. https://xdsoft.net/jodit/pro/
