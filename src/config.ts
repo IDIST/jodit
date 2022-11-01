293bac6b1712bce6ad83410de3a3011c0b7269a2
Sunwook KIm <sunus7@naver.com>
2022-11-01 22:57:41 +0900

    수정: 색상 리스트 수정, 디바운스 시간 설정

--- src/config.ts
+++ src/config.ts
@@ -60,7 +60,7 @@ export class Config implements IViewOptions {
 	/**
 	 * Timeout of all asynchronous methods
 	 */
-	defaultTimeout: number = 100;
+	defaultTimeout: number = 500;
 
 	namespace: string = '';
 
@@ -383,7 +383,13 @@ export class Config implements IViewOptions {
 			'Drop gif': 'GIF 드래그',
 			'Drop audio': '오디오 드래그',
 			'Search image': '이미지 검색',
-			'Search gif': 'Gif 검색'
+			'Search gif': 'Gif 검색',
+			'Powered by: %s': '라이센스: %s',
+
+			'By using images from Unsplash, you agree to the License and Terms of Service.':
+				'Unsplash의 이미지를 사용하면 라이선스 및 서비스약관에 동의하는 것입니다.',
+			'By using gif images from Giphy, you agree to the License and Terms of Service.':
+				'Giphy의 이미지를 사용하면 라이선스 및 서비스약관에 동의하는 것입니다.'
 		}
 	};
 
@@ -502,91 +508,32 @@ export class Config implements IViewOptions {
 	 * ```
 	 */
 	colors: IDictionary<string[]> | string[] = {
-		greyscale: [
-			'#000000',
-			'#434343',
-			'#666666',
-			'#999999',
-			'#B7B7B7',
-			'#CCCCCC',
-			'#D9D9D9',
-			'#EFEFEF',
-			'#F3F3F3',
-			'#FFFFFF'
+		dark: [
+			'#182742',
+			'#0F4196',
+			'#2A8099',
+			'#1F593E',
+			'#F59237',
+			'#AC3017',
+			'#343084'
 		],
-		// palette: [
-		// 	'#980000',
-		// 	'#FF0000',
-		// 	'#FF9900',
-		// 	'#FFFF00',
-		// 	'#00F0F0',
-		// 	'#00FFFF',
-		// 	'#4A86E8',
-		// 	'#0000FF',
-		// 	'#9900FF',
-		// 	'#FF00FF'
-		// ],
-		full: [
-			// '#E6B8AF',
-			// '#F4CCCC',
-			// '#FCE5CD',
-			// '#FFF2CC',
-			// '#D9EAD3',
-			// '#D0E0E3',
-			// '#C9DAF8',
-			// '#CFE2F3',
-			// '#D9D2E9',
-			// '#EAD1DC',
-			'#DD7E6B',
-			'#EA9999',
-			'#F9CB9C',
-			'#FFE599',
-			'#B6D7A8',
-			'#A2C4C9',
-			'#A4C2F4',
-			'#9FC5E8',
-			'#B4A7D6',
-			'#D5A6BD',
-			'#CC4125',
-			'#E06666',
-			'#F6B26B',
-			'#FFD966',
-			'#93C47D',
-			'#76A5AF',
-			'#6D9EEB',
-			'#6FA8DC',
-			'#8E7CC3',
-			'#C27BA0',
-			'#A61C00',
-			'#CC0000',
-			'#E69138',
-			'#F1C232',
-			'#6AA84F',
-			'#45818E',
-			'#3C78D8',
-			'#3D85C6',
-			'#674EA7',
-			'#A64D79',
-			'#85200C',
-			'#990000',
-			'#B45F06',
-			'#BF9000',
-			'#38761D',
-			'#134F5C',
-			'#1155CC',
-			'#0B5394',
-			'#351C75',
-			'#733554',
-			'#5B0F00',
-			'#660000',
-			'#783F04',
-			'#7F6000',
-			'#274E13',
-			'#0C343D',
-			'#1C4587',
-			'#073763',
-			'#20124D',
-			'#4C1130'
+		middle: [
+			'#8D96A4',
+			'#4A91F7',
+			'#38ADD0',
+			'#4AA677',
+			'#FABD39',
+			'#F15836',
+			'#564FB1'
+		],
+		light: [
+			'#FFFFFF',
+			'#ADCEFB',
+			'#B3F1FD',
+			'#ADF0CD',
+			'#FEEDB1',
+			'#F8B8A6',
+			'#E6E3FD'
 		]
 	};
 
