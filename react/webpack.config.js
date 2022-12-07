365288d226b3eeff6ad6055d9456054721f7dc9b
Sunwook KIm <sunus7@naver.com>
2022-12-08 00:59:03 +0900

    no message

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -3,31 +3,78 @@
  * See LICENSE.md in the project root for license information.
  * Copyright (c) 2013-2022 Valeriy Chupurnov. All rights reserved. https://xdsoft.net/jodit/pro/
  */
-const config = require('jodit-react/webpack.config');
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = (env, argv) => {
-	const defaultConfig = config(env, argv, __dirname);
+module.exports = (env, argv, dir = process.cwd()) => {
+	const debug = !argv || !argv.mode || !argv.mode.match(/production/);
 
 	return {
-		...defaultConfig,
+		context: dir,
+
+		entry: './src/index.js',
+		devtool: debug ? 'inline-source-map' : false,
+
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					use: {
+						loader: 'babel-loader',
+						options: require(path.join(dir, './babel.config.json'))
+					}
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				}
+			]
+		},
+
 		resolve: {
-			...defaultConfig.resolve,
 			alias: {
-				...defaultConfig.resolve.alias,
+				'jodit-react': path.join(__dirname, './src'),
 				'jodit-pro': path.join(__dirname, '..', './')
 			}
 		},
+
+		output: {
+			path: path.join(dir, './build/'),
+			filename: 'jodit-react.js',
+			library: ['JoditEditor', 'Jodit'],
+			libraryTarget: 'umd'
+		},
+
 		plugins: [
-			...defaultConfig.plugins,
+			new webpack.DefinePlugin({
+				'process.env': {
+					NODE_ENV: JSON.stringify(
+						debug ? 'development' : 'production'
+					)
+				}
+			}),
+			new webpack.optimize.ModuleConcatenationPlugin(),
 			new webpack.NormalModuleReplacementPlugin(
 				/include\.jodit\.js/,
 				path.join(__dirname, './include.jodit.js')
 			)
 		],
+
 		externals: {
-			...defaultConfig.externals,
+			jodit: 'jodit',
+			Jodit: 'Jodit',
+			react: {
+				root: 'React',
+				commonjs2: 'react',
+				commonjs: 'react',
+				amd: 'react'
+			},
+			'react-dom': {
+				root: 'ReactDOM',
+				commonjs2: 'react-dom',
+				commonjs: 'react-dom',
+				amd: 'react-dom'
+			},
 			'jodit-pro/build/jodit.fat.js': 'jodit-pro/build/jodit.fat.js'
 		}
 	};
